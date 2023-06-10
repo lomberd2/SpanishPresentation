a488c981c082d71d9b7b9e31228f3c6928787062
lomberd2 <joeyblankenhagen@gmail.com>
2023-06-10 11:55:44 +0200

    Fixed audioplayer bug

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component} from '@angular/core';
+import {Song} from "../../interfaces/song";
 
 @Component({
   selector: 'app-audio-player',
@@ -38,8 +39,7 @@ export class AudioPlayerComponent  implements AfterViewInit {
     // get the audio element with id audio
     const audio = document.getElementById('audio') as HTMLAudioElement;
 
-    // set the audio volume
-    audio.volume = 0.05;
+
 
     // get an interaction of the user with the document first
     document.addEventListener('click', () => {
@@ -53,50 +53,57 @@ export class AudioPlayerComponent  implements AfterViewInit {
   }
 
   public static play() {
-    const interpret = document.getElementById('interpret') as HTMLSpanElement;
-    const audio = document.getElementById('audio') as HTMLAudioElement;
-    const original = document.getElementById('original') as HTMLElement;
+    try {
+      const interpret = document.getElementById('interpret') as HTMLSpanElement;
+      const audio = document.getElementById('audio') as HTMLAudioElement;
+      const original = document.getElementById('original') as HTMLElement;
 
-    interpret.innerText = this.currentSong.artist + ' - ' + this.currentSong.name;
-    audio.src = this.currentSong.src;
-    // @ts-ignore
-    original.href = this.currentSong.url_origin;
+      // set the audio volume
+      audio.volume = 0.05;
 
-    audio.play();
+      interpret.innerText = this.currentSong.artist + ' - ' + this.currentSong.name;
+      audio.src = this.currentSong.src;
+      // @ts-ignore
+      original.href = this.currentSong.url_origin;
+
+      audio.play();
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   public static pause() {
-    const audio = document.getElementById('audio') as HTMLAudioElement;
-    audio.pause();
-    this.isPaused = true;
+    try {
+      const audio = document.getElementById('audio') as HTMLAudioElement;
+      audio.pause();
+      this.isPaused = true;
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   public static toggleVisibility() {
-    if (AudioPlayerComponent.isHidden_) {
-      AudioPlayerComponent.show();
+    if (this.isHidden_) {
+      this.show();
     } else {
-      AudioPlayerComponent.hide();
+      this.hide();
     }
   }
 
   public static hide() {
-    AudioPlayerComponent.isHidden_ = true;
+    this.isHidden_ = true;
   }
 
   public static show() {
-    AudioPlayerComponent.isHidden_ = false;
-    this.play();
+    this.isHidden_ = false;
+
+    setTimeout(() => {
+      this.play();
+    }, 250);
   }
 
-  public setNextSong(song: Song) {
-
+  public static setNextSong(song: Song) {
+    this.currentSong = song;
   }
 
-}
-
-interface Song {
-  name: string;
-  artist: string;
-  url_origin: string;
-  src: string;
 }
