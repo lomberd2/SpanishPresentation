3db3b99c462394140d10e7dd1968a3cbc3dad60c
lomberd2 <joeyblankenhagen@gmail.com>
2023-06-10 18:05:47 +0200

    Fix for release

--- src/app/guards/is-logged-in.guard.ts
+++ src/app/guards/is-logged-in.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 import {UserManagerService} from "../services/user-manager.service";
+import {IS_DEV} from "../../main";
 
 export const isLoggedInGuard: CanActivateFn = (route, state) => {
   if (UserManagerService.isLoggedIn()) {
@@ -7,7 +8,7 @@ export const isLoggedInGuard: CanActivateFn = (route, state) => {
   }
   // Redirect to the landing page if the user isnt logged in
   setTimeout(() => {
-    window.location.href = '/';
+    window.location.href = '/spanish-presentation/';
     window.location.reload();
   }, 100);
   return false;
